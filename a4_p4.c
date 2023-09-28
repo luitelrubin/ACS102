6cc23276d0d4ef46b6631f34e92a68233152d1fd
rubin <rubinprasadluitel@gmail.com>
2023-09-28 21:23:50 +0545

    in a4_p4 remove strlwr() function and replace that with tolower() function from ctype.h header file

--- a4_p4.c
+++ a4_p4.c
@@ -7,19 +7,18 @@ rluitel@jacobs-university.de
 
 #include <stdio.h>
 #include <string.h>
-
+#include <ctype.h>
 int count_consonants(char str[]);
 
 int count_consonants(char str[])
 {
     int vowels[] = {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0};
     int cons_count = 0;
-    strlwr(str); // make all alphabets in str lowercase
 
     int charnum;
     for (int i = 0; i < strlen(str); i++)
     {
-        charnum = str[i] - 'a'; // charnum is 0 for a and 25 for z
+        charnum = tolower(str[i]) - 'a'; // charnum is 0 for a and 25 for z
 
         if (charnum >= 0 && charnum < 26)
         {
